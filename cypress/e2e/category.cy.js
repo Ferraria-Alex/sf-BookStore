c3a789131dc45051a4c491db3a9d0d0a5a1478a6
Ferraria-Alex <alexmferrari.a@gmail.com>
2025-03-27 16:46:29 +0100

    Category Update Tests And Redo of Delete All Test Data SemiTest

---
+++ cypress/e2e/category.cy.js
@@ -0,0 +1,78 @@
+describe('Category Tests', () => {
+  it('Adding a new category - SUCCESS', () => {
+    //Arrange
+    cy.visit('http://127.0.0.1:8000/category/new');
+    cy.wait(300);
+    //Act
+    cy.get('#category_label').type('Exemple1');
+    cy.get('.btn').click();
+    cy.wait(300);
+    //Assert
+    cy.get('h1').contains('Category index');
+    cy.contains('table tr', 'Exemple1').should('exist');
+  })
+
+  it('Adding new category - Duplicated entry error', () => {
+    //Arrange
+    cy.visit('http://127.0.0.1:8000/category/new');
+    cy.wait(300);
+    //Act
+    cy.get('#category_label').type('Exemple1');
+    cy.get('.btn').click();
+    cy.wait(300);
+    //Assert
+    cy.get('.container > .break-long-words').contains("An exception");
+  })
+
+  it('Update an entry - SUCCCESS', () =>{
+    cy.visit('http://127.0.0.1:8000/category/new');
+    cy.wait(300);
+    cy.get('#category_label').type('Exemple2');
+    cy.get('.btn').click();
+    cy.wait(300);
+    cy.visit('http://127.0.0.1:8000/category');
+    cy.get('tbody > :nth-child(2) > :nth-child(3) > :nth(1)').click();
+    cy.wait(300);
+    cy.get('#category_label').clear().type('Exemple2.1');
+    cy.get('[name="category"] > .btn').click();
+    cy.wait(300);
+    cy.get('h1').contains('Category index');
+    cy.get('.table').contains('Exemple2.1');
+  })
+
+  it('Update an entry - Duplicated entry error', () => {
+    //Arrange
+    cy.visit('http://127.0.0.1:8000/category');
+    cy.get('tbody > :nth-child(1) > :nth-child(3) > :nth(1)').click();
+    cy.wait(300);
+    //Act
+    cy.get('#category_label').clear().type('Exemple2.1');
+    cy.get('[name="category"] > .btn').click();
+    cy.wait(300);
+    //Assert
+    cy.get('.container > .break-long-words').contains("An exception");
+  })
+
+  it('Deletes All Test Data', ()=>{
+    //RESET
+    cy.visit('http://127.0.0.1:8000/category');
+    clickVisibleButton();
+  })
+
+  function clickVisibleButton() {
+    cy.get('tbody').then( $mainContainer => {
+        const isVisible = $mainContainer.find(':nth-child(1) > :nth-child(3) > :nth(0)').is( ':visible' );
+        if (isVisible) {
+          cy.wait(300);
+          cy.get('tbody > :nth-child(1) > :nth-child(3) > :nth(0)').click();
+          cy.wait(300);
+          cy.get('.btn').click();
+          cy.wait(300);
+          clickVisibleButton();
+        } else {
+          return;
+        }
+    });
+  }
+
+})
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -32,13 +32,12 @@ final class CategoryController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($category);
             $entityManager->flush();
-
             return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('category/new.html.twig', [
             'category' => $category,
-            'form' => $form,
+            'form' => $form
         ]);
     }
 
